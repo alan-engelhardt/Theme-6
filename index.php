86f27d49970ab2738cdbb5664fbbcb3480c94f6f
juliojphg <jphg.kea@gmail.com>
2016-08-15 13:06:59 +0200

    Revert "ændret fra content til Content"
    This reverts commit 49244a93de6d17b4494b647e01948c46bba06a8f.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ session_start();
 $css = "css/style.css";
 $title = "This is my new public page";
 $header = "<h1 class='public'>This is my public page</h1>";
-$Content = "";
+$content = "";
 $loginForm = "<p><form method='post' action=''>
                 <label for='user'>Username</label>
                 <input type='text' id='user' name='username' required>
@@ -25,7 +25,7 @@ if(isset($_SESSION['user_ID'])){
     $nav ="<a href='../index.php'>Frontpage</a>
         <a href='#'>Page 2</a>
         <a href='#'>Page 3</a>";
-    $Content .= "<h1>Please log in</h1> $loginForm";
+    $content .= "<h1>Please log in</h1> $loginForm";
 }
 
 if(isset($_POST['login']) && !empty($_POST['username']) && !empty($_POST['password'])){
@@ -46,11 +46,11 @@ if(isset($_POST['login']) && !empty($_POST['username']) && !empty($_POST['passwo
         $_SESSION['user_image'] = $validUser->image;
         header('Location: admin/admin.php');
     }else{
-        $Content = "<h1>Login failed. Please try again.</h1> $loginForm";
+        $content = "<h1>Login failed. Please try again.</h1> $loginForm";
     }
 
 }else{
-    $Content .= "<p>This is public </p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis lectus quis sem lacinia nonummy. Proin mollis lorem non dolor. In hac habitasse platea dictumst. Nulla ultrices odio. Donec augue. Phasellus dui. Maecenas facilisis nisl vitae nibh. Proin vel est vitae eros pretium dignissim. Aliquam aliquam sodales orci. Suspendisse potenti. Nunc adipiscing euismod arcu. Quisque facilisis mattis lacus. Fusce bibendum, velit in venenatis viverra, tellus ligula dignissim felis, quis euismod mauris tellus ut urna. Proin scelerisque. Nulla in mi. Integer ac leo. Nunc urna ligula, gravida a, pretium vitae, bibendum nec, ante. Aliquam ullamcorper iaculis lectus. Sed vel dui. Etiam lacinia risus vitae lacus. Aliquam elementum imperdiet turpis. In id metus. Mauris eu nisl. Nam pharetra nisi nec enim. Nulla aliquam, tellus sed laoreet blandit, eros urna vehicula lectus, et vulputate mauris arcu ut arcu. Praesent eros metus, accumsan a, malesuada et, commodo vel, nulla. Aliquam sagittis auctor sapien. Morbi a nibh.</p>";
+    $content .= "<p>This is public </p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis lectus quis sem lacinia nonummy. Proin mollis lorem non dolor. In hac habitasse platea dictumst. Nulla ultrices odio. Donec augue. Phasellus dui. Maecenas facilisis nisl vitae nibh. Proin vel est vitae eros pretium dignissim. Aliquam aliquam sodales orci. Suspendisse potenti. Nunc adipiscing euismod arcu. Quisque facilisis mattis lacus. Fusce bibendum, velit in venenatis viverra, tellus ligula dignissim felis, quis euismod mauris tellus ut urna. Proin scelerisque. Nulla in mi. Integer ac leo. Nunc urna ligula, gravida a, pretium vitae, bibendum nec, ante. Aliquam ullamcorper iaculis lectus. Sed vel dui. Etiam lacinia risus vitae lacus. Aliquam elementum imperdiet turpis. In id metus. Mauris eu nisl. Nam pharetra nisi nec enim. Nulla aliquam, tellus sed laoreet blandit, eros urna vehicula lectus, et vulputate mauris arcu ut arcu. Praesent eros metus, accumsan a, malesuada et, commodo vel, nulla. Aliquam sagittis auctor sapien. Morbi a nibh.</p>";
 }
 
 include_once "inc/template.php";
